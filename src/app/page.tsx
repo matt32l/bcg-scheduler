0ec293f24a9cf1f91965317b3a6a8a0ea0a9c298
matt32l <47435720+matt32l@users.noreply.github.com>
2025-01-25 15:21:59 -0500

    Add main page component

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import BCGScheduleGenerator from './components/BCGScheduleGenerator';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen py-8">
+      <BCGScheduleGenerator />
+    </main>
+  );
+}
